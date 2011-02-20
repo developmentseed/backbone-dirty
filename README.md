ee61e5a1a159b9d6aea1bdf4e2e3e92d4cd9f5a7
Young Hahn <young@developmentseed.org>
2011-02-20 12:43:59 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,43 @@
+Backbone Dirty
+--------------
+Server-side overrides for Backbone to use `node-dirty` for Model persistence.
+
+### Compatibility
+
+Backbone 0.3.3.
+
+### Usage
+
+Pass the `Backbone` object and a filepath to the db (will be created if it
+doesn't exist yet) when calling `require()`.
+
+    var Backbone = require('backbone');
+    require('backbone-dirty')(Backbone, 'app.db');
+
+    // Backbone.sync will now load and save models from app.db.
+
+### Conventions
+
+`backbone-dirty` stores models in the `node-dirty` db using the `model.url` as
+its key. Collections retrieve models by matching the Collection url against
+the initial portion of the Model url.
+
+    var orange = new FruitModel({id: 'orange'});
+    var apple = new FruitModel({id: 'apple'});
+    var banana = new FruitModel({id: 'banana'});
+
+    console.log(orange.url()); // fruits/orange
+    console.log(apple.url());  // fruits/apple
+    console.log(banana.url()); // fruits/banana
+
+    var fruits = new FruitCollection();
+
+    console.log(fruits.url);   // fruits
+
+    fruits.fetch();            // retrieves orange, apple, banana
+
+### Authors
+
+- [Will White](http://github.com/willwhite)
+- [Young Hahn](http://github.com/yhahn)
+
---
+++ backbone-dirty.js
@@ -0,0 +1,82 @@
+// Exports Backbone with `Backbone.sync()` overridden for the server-side
+// context. Uses `node-dirty` for model persistence. Models are expected to
+// have a URL prefixed by their respective collection (e.g. `/{class}/{id}`)
+// and Collections retrieve their respective models based on this convention.
+module.exports = function(Backbone, filename) {
+    var dirty = require('node-dirty')(filename),
+        loaded = false;
+
+    // Helper function to get a URL from a Model or Collection as a property
+    // or as a function.
+    var getUrl = function(object) {
+        if (object.url instanceof Function) {
+            return object.url();
+        } else if (typeof object.url === 'string') {
+            return object.url;
+        }
+    };
+
+    // Sync implementation for `node-dirty`.
+    var sync = function(method, model, success, error) {
+        switch (method) {
+        case 'read':
+            var data,
+                base;
+            if (model.id) {
+                data = dirty.get(getUrl(model));
+                if (data) {
+                    success(data);
+                } else {
+                    error('Model not found.');
+                }
+            } else {
+                data = [];
+                base = getUrl(model);
+                dirty.forEach(function(key, val) {
+                    if (val && key.indexOf(base) === 0 && data.indexOf(val) === -1) {
+                        data.push(val);
+                    }
+                });
+                success(data);
+            }
+            break;
+        case 'create':
+        case 'update':
+            dirty.set(
+                getUrl(model),
+                model.toJSON(),
+                function(err, model) {
+                    return err ? error(err) : success(model);
+                }
+            );
+            break;
+        case 'delete':
+            dirty.rm(
+                getUrl(model),
+                function(err, model) {
+                    return err ? error(err) : success(model);
+                }
+            );
+            break;
+        }
+    };
+
+    // Set a loaded flag to indicate whether `Backbone.sync()` can begin accessing
+    // the db immediately or must wait until the `load` event is emitted.
+    dirty.on('load', function() {
+        loaded = true;
+    });
+
+    // Override Backbone.sync. Defer the sync operation until the database has
+    // been fully loaded if necessary.
+    Backbone.sync = function(method, model, success, error) {
+        if (loaded) {
+            sync(method, model, success, error);
+        } else {
+            dirty.on('load', function() {
+                sync(method, model, success, error);
+            });
+        }
+    };
+};
+
