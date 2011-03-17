1e98634d933815222cc055f387afd70a7604d9ff
Young Hahn <young@developmentseed.org>
2011-03-17 11:58:31 -0400

    Remove fixtures feature and don't override Backbone.sync globally by default. Fixes #1.

--- README.md
+++ README.md
@@ -11,33 +11,10 @@ Backbone 0.3.3.
 Pass a filepath to the db (will be created if it doesn't exist yet) when
 calling `require()`.
 
-    var dirty = require('backbone-dirty')('app.db');
     var Backbone = require('backbone');
+    Backbone.sync = require('backbone-dirty')('app.db');
 
     // Backbone.sync will now load and save models from app.db.
-
-### Fixtures
-
-An optional array of fixtures can be passed to load default data to be
-retrieved. Loaded fixtures cannot be deleted but may be "overridden" by a
-database-saved version of the same model. The fixtures array may contain
-
-1. An object with `key` and `val` attributes
-2. A string path to a JSON file containing `1`
-3. A string path to a directory containing `2`
-
-    var fixtures = [
-        '/path/to/a/file.json',
-        '/path/to/many/fixtures',
-        {
-            key: 'fruits/orange',
-            val: {
-                id: 'orange',
-                flavor: 'delicious'
-            }
-        }
-    ];
-    var dirty = require('backbone-dirty')('app.db', fixtures);
 
 ### Conventions
 
--- backbone-dirty.js
+++ backbone-dirty.js
@@ -1,19 +1,14 @@
-// Overrides Backbone with `Backbone.sync()` overridden for the server-side
+// Provides a `Backbone.sync` or `Model.sync` method for the server-side
 // context. Uses `node-dirty` for model persistence. Models are expected to
 // have a URL prefixed by their respective collection (e.g. `/{class}/{id}`)
 // and Collections retrieve their respective models based on this convention.
 var _ = require('underscore')._,
     Backbone = require('backbone'),
-    fs = require('fs'),
-    path = require('path'),
-    dirty,
-    fixtures,
-    loaded = false;
+    loaded = {},
+    dbs = {};
 
-module.exports = function(filename, fixtureList) {
-    dirty = dirty || require('node-dirty')(filename);
-    fixtureList = fixtureList || [];
-    fixtureList = _.isString(fixtureList) && [fixtureList];
+module.exports = function(filename) {
+    var dirty = dbs[filename] = dbs[filename] || require('node-dirty')(filename);
 
     // Helper function to get a URL from a Model or Collection as a property
     // or as a function.
@@ -25,54 +20,19 @@ module.exports = function(filename, fixtureList) {
         }
     };
 
-    // Load all fixtures at initial require time. Blocking.
-    var loadFixture = function(fixture) {
-        if (_.isString(fixture)) {
-            try {
-                var stats = fs.statSync(fixture);
-                if (stats.isFile()) {
-                    loadFixture(JSON.parse(fs.readFileSync(fixture)));
-                } else if (stats.isDirectory()) {
-                    _.each(fs.readdirSync(fixture), function(file) {
-                        loadFixture(path.join(fixture, file));
-                    });
-                }
-            } catch(e) {}
-        } else if ('key' in fixture && 'val' in fixture) {
-            fixtures[fixture.key] = fixture.val;
-        }
-    }
-    fixtures || ((fixtures = {}) && _.each(fixtureList, loadFixture));
-
     // Sync implementation for `node-dirty`.
     var sync = function(method, model, success, error) {
         switch (method) {
         case 'read':
             var data,
-                base,
-                found;
+                base = getUrl(model);
             if (model.id) {
-                data = dirty.get(getUrl(model)) || fixtures[getUrl(model)];
-                if (data) {
-                    success(data);
-                } else {
-                    error('Model not found.');
-                }
+                data = dirty.get(base);
+                (data && success(data)) || error('Model not found.');
             } else {
                 data = [];
-                found = [];
-                base = getUrl(model);
                 dirty.forEach(function(key, val) {
-                    if (val && key.indexOf(base) === 0 && data.indexOf(val) === -1) {
-                        data.push(val);
-                        found.push(key);
-                    }
-                });
-                _.each(fixtures, function(val, key) {
-                    if (val && key.indexOf(base) === 0 && data.indexOf(val) === -1 && found.indexOf(key) === -1) {
-                        data.push(val);
-                        found.push(key);
-                    }
+                    val && key.indexOf(base) === 0 && data.indexOf(val) === -1 && data.push(val);
                 });
                 success(data);
             }
@@ -104,22 +64,13 @@ module.exports = function(filename, fixtureList) {
         }
     };
 
-    // Set a loaded flag to indicate whether `Backbone.sync()` can begin accessing
+    // Set a loaded flag to indicate whether sync can begin accessing
     // the db immediately or must wait until the `load` event is emitted.
-    dirty.on('load', function() { loaded = true });
+    dirty.on('load', function() { loaded[filename] = true });
 
-    // Override Backbone.sync. Defer the sync operation until the database has
-    // been fully loaded if necessary.
-    Backbone.sync = function(method, model, success, error) {
-        if (loaded) {
-            sync(method, model, success, error);
-        } else {
-            dirty.on('load', function() {
-                sync(method, model, success, error);
-            });
-        }
+    return function(method, model, success, error) {
+        var deferred = function() { sync(method, model, success, error) };
+        loaded[filename] ? deferred() : dirty.on('load', deferred);
     };
-
-    return dirty;
 };
 
